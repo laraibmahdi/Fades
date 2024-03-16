1adf8a7cee34461765a0b2446ee6bf2b21da62aa
Laraib Mahdi <laraib219@gmail.com>
2024-03-16 00:27:22 -0500

    logout button implemented

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from '@firebase/auth';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAGqdwWQzpWvGLP-zaFCq1cnY6jPE4EHxw",
+  authDomain: "hair-486ae.firebaseapp.com",
+  projectId: "hair-486ae",
+  storageBucket: "hair-486ae.appspot.com",
+  messagingSenderId: "476959662144",
+  appId: "1:476959662144:web:3d3cf864d3634a4a3abfa5",
+  measurementId: "G-R10YS1L15D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- navigation/screens/Settings.js
+++ navigation/screens/Settings.js
@@ -1,12 +1,55 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { auth } from '../../firebase';
 
-export default function SettingsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Settings Screen</Text>
-        </View>
-    );
-}
+const SettingsScreen = () => {
+  const navigation = useNavigation();
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.replace("Login");
+      })
+      .catch(error => alert(error.message));
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.email}>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  email: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '60%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
+
+export default SettingsScreen;
